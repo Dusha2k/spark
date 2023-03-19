c8238105aaea30a26e70987b09c7a38220c08498
Dusha2k <efimov.12@mail.ru>
2023-03-19 22:13:24 +0300

    fr: added styles for NProgress

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider, theme } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import router from 'next/router';
 import { useState } from 'react';
+import 'nprogress/nprogress.css';
 import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 
 export default function App({ Component, pageProps, router }: AppProps) {
