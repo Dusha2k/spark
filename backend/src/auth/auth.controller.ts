fd08794ff648d1ac527f9f73097b673ddc066b4b
Dusha2k <efimov.12@mail.ru>
2023-03-19 17:36:38 +0300

    be: add swagger

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -6,10 +6,12 @@ import {
   Param,
   ConflictException,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Spark')
+    .setDescription('Spark api')
+    .setVersion('1.0')
+    .addTag('spark')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(7777);
 }
 bootstrap();
