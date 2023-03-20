dd1f5f8de025bb6391a1effa53348100508769f4
Dusha2k <efimov.12@mail.ru>
2023-03-20 23:21:00 +0300

    fr: generate openAPI

---
+++ frontend/src/shared/openapi/index.ts
@@ -0,0 +1,15 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { LoginDto } from './models/LoginDto';
+export type { LoginUserResponseDto } from './models/LoginUserResponseDto';
+export type { OmitTypeClass } from './models/OmitTypeClass';
+export type { RegisterDto } from './models/RegisterDto';
+
+export { AuthService } from './services/AuthService';
+export { DefaultService } from './services/DefaultService';
