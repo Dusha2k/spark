c3e579a71802bcbaec9fe3f7c7ab370eae4e6bec
Dusha2k <efimov.12@mail.ru>
2023-03-26 21:19:08 +0300

    fr: add app page detail by id

--- frontend/src/modules/messagesList/MessagesList.tsx
+++ frontend/src/modules/messagesList/MessagesList.tsx
@@ -1,3 +1,24 @@
-export const MessagesList = () => {
-  return <></>;
+import { messageAPI } from '@/shared/api';
+import { Flex, Text } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+
+export const MessagesList = ({ channelId }: { channelId: number }) => {
+  const { data, isLoading } = useQuery(['messages', channelId], () =>
+    messageAPI.messageControllerFindByChannelId(channelId.toString()),
+  );
+
+  return (
+    <Flex direction="column">
+      {!isLoading && data?.data?.length > 0 ? (
+        data?.data?.map((message) => (
+          <Flex direction="column" key={message.id}>
+            <Text>{message.owner.login}</Text>
+            <Text>{message.text}</Text>
+          </Flex>
+        ))
+      ) : (
+        <Text>Нихера нет</Text>
+      )}
+    </Flex>
+  );
 };
--- frontend/src/modules/usersList/UsersList.tsx
+++ frontend/src/modules/usersList/UsersList.tsx
@@ -1,3 +1,59 @@
+import { channelAPI, userAPI } from '@/shared/api';
+import { Image } from '@/shared/components';
+import {
+  Flex,
+  Text,
+  Popover,
+  PopoverContent,
+  Portal,
+  PopoverTrigger,
+  PopoverBody,
+} from '@chakra-ui/react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+
 export const UsersList = () => {
-  return <></>;
+  const { data, isLoading } = useQuery(['users'], () =>
+    userAPI.userControllerGetAllUsers(),
+  );
+
+  const { mutate } = useMutation((members: string[]) =>
+    channelAPI.channelControllerCreate({
+      members,
+    }),
+  );
+
+  return (
+    <Flex padding="15px" gap={3} alignItems="flex-start" direction="column">
+      {!isLoading && (
+        <>
+          {data?.data?.map((user) => (
+            <Popover key={user.id}>
+              <PopoverTrigger>
+                <Flex cursor="pointer" gap={3} alignItems="flex-start">
+                  <Image
+                    alt="avatar"
+                    src="/discord-avatar.png"
+                    //objectFit="contain"
+                    width={35}
+                    height={40}
+                    w="40px"
+                    h="40px"
+                    rounded="full"
+                  />
+                  <Text>{user.login}</Text>
+                </Flex>
+              </PopoverTrigger>
+              <Portal>
+                <PopoverContent>
+                  <PopoverBody onClick={() => mutate(['14', '15'])}>
+                    Написать сообщение
+                  </PopoverBody>
+                </PopoverContent>
+              </Portal>
+            </Popover>
+          ))}
+        </>
+      )}
+    </Flex>
+  );
 };
