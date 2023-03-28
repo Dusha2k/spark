da24d2f4249f85ec3efc0b9254d4901ca93eefcf
Dusha2k <efimov.12@mail.ru>
2023-03-28 16:52:00 +0300

    be: change origin port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({
     allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
